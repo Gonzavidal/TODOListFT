ce8151359f06a3ffdde59d562cda4950e63ad844
Gonzavidal <gvidal1992@gmail.com>
2023-03-21 00:56:07 +0000

    Avance final revisión

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,44 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import List from "./List.jsx";
 
 const Home = () => {
-  const [task, setTask] = useState("");
+  const [task, setTask] = useState({label: "", done: false, });
   const [list, setList] = useState([]);
+  console.log("list", list);
 
-   
+
   const handleChange = (event) => {
-    setTask(event.target.value);
+    setTask({label: event.target.value, done: false,});
   };
+
+  const getList = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/gonzavidal", {method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("data", data);
+      });
+  }
+
+  const createList = (list) => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/gonzavidal", {
+      method: "PUT",
+      headers: {"Content-Type": "application/json",},
+      body: JSON.stringify(list),
+    });
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
 
   const onSubmit = (event) => {
     event.preventDefault();
     setList([...list, task]);
-    setTask("");
+    createList([...list, task]);
+    setTask({label: "",done: false,
+    });
   };
 
   const onDelete = (item) => {
-    const filteredList = list.filter((listItem) => {
-      return listItem !== item;
+    const filteredTodoList = list.filter((todoItem) => {
+      return todoItem.label !== item.label;
     });
-    setList(filteredList);
-  };
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/gonzavidal", {
+      method: "PUT",
+      headers: {"Content-Type": "application/json",},
+      body: JSON.stringify(filteredTodoList),
+    });
 
-  return (
-    <div className="form">
-      <h1 className="title">todos</h1>
-      <form onSubmit={onSubmit}>
-        <input type="text" placeholder="What needs to be done?"
-          value={task}
-          onChange={handleChange}
-        />
-      </form>
-      
+    const deleteAll = () => {
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/gonzavidal", {
+        method: "PUT",
+        headers: {"Content-Type": "application/json",},
+        body: JSON.stringify([{}]),
+      });
+      setList([]);
+    };
+
+    return (
+      <div className="form">
+        <h1 className="title">todos</h1>
+        <form onSubmit={onSubmit}>
+          <input type="text" placeholder="What needs to be done?"
+            value={task}
+            onChange={handleChange}
+          />
+        </form>
+
         {list.map((item, index) => {
           return <List ListItem={item} deleteList={onDelete} key={index} />;
         })}
-      
-    </div>
-  );
-};
 
+      </div>
+    );
+  };
+};
 export default Home;
